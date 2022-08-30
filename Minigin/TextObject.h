d6a281a29a3007f40466496c8b47b61cdad8bef7
grrava <alex.vandenabeele@gmail.com>
2022-08-30 12:07:03 +0200

    enabled code analysis on build

--- Minigin/TextObject.h
+++ Minigin/TextObject.h
@@ -24,7 +24,7 @@ namespace dae
 	private:
 		bool m_NeedsUpdate;
 		std::string m_Text;
-		Transform m_Transform;
+		Transform m_Transform{};
 		std::shared_ptr<Font> m_Font;
 		std::shared_ptr<Texture2D> m_TextTexture;
 	};
